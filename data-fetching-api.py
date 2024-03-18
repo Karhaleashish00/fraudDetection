93d9a8ac7bfbfac1a070ba533ef0255745460570
Ashish <karhaleashish007@gmail.com>
2024-03-18 11:45:44 +0530

    comments added

--- data-fetching-api.py
+++ data-fetching-api.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify
 import parser
 
-
 app = Flask(__name__)
 
-@app.route('/get-parsed-data')
-def get_parse_data():
-    dict = parser.get_parsed_data()
+
+@app.route("/get-parsed-data")  # This is the API route to extract college name from resume
+def get_data():
+    dict = parser.get_parsed_data() # get_parsed_data() function is defined in the parser.py
     return dict
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
